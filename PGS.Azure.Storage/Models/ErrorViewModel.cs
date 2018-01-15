75c9d1e953b5d732f995360db1cc7bf2dbd29c67
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-15 14:57:23 +0100

    Default MVC project

---
+++ PGS.Azure.Storage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PGS.Azure.Storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
