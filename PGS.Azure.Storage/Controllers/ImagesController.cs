63889f730abab0528eeca76f388bd1f0587c29ba
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-15 15:04:21 +0100

    Image gallery page

---
+++ PGS.Azure.Storage/Controllers/ImagesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PGS.Azure.Storage.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
